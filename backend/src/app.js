fe831fc163b36dc29c734a5ff1831120732189e4
Aidan McCarthy <aidanmccarthy3@gmail.com>
2026-03-01 17:55:02 +0000

    Add CORS package to app.js for improved configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const authRoutes = require("./routes/authRoutes");
 const profileRoutes = require("./routes/profileRoutes");
